6fa05bd016ec8c72a6bd3316d2563ddbd28bc04c
freetersubs <freetersubs1@gmail.com>
2023-11-26 09:23:16 +0700

    Update

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
-// Toggle class active
-const navbarNav = document.querySelector('.navbar-nav');
+// // Toggle class active
+// const navbarNav = document.querySelector('.navbar-nav');
 
-// Ketika hamburger menu diklik
-document.querySelector('#hamburger-menu').onclick = () => {
-  navbarNav.classList.toggle('active');
-};
+// // Ketika hamburger menu diklik
+// document.querySelector('#hamburger-menu').onclick = () => {
+//   navbarNav.classList.toggle('active');
+// };
 
-// Klik di manapun untuk menghilangkan sidebar
-const hamburger = document.querySelector('#hamburger-menu');
+// // Klik di manapun untuk menghilangkan sidebar
+// const hamburger = document.querySelector('#hamburger-menu');
 
-document.addEventListener('click', function (e) {
-  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
-    navbarNav.classList.remove('active');
-  }
-});
+// document.addEventListener('click', function (e) {
+//   if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+//     navbarNav.classList.remove('active');
+//   }
+// });
